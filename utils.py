392c5edf39da4fd38ee30a15a355f2bb739034ad
julykobe <myzswen@gmail.com>
2014-10-09 09:39:37 +0800

    in process
    add *.log to gitignore

---
+++ utils.py
@@ -0,0 +1,18 @@
+from __future__ import with_statement
+import ConfigParser
+
+CONF_FILE = 'autoscaling.conf'
+
+def get_config(block, key):
+
+	#get parser
+	config = ConfigParser.ConfigParser()
+
+	#open config file
+	with open(CONF_FILE, 'r') as conf_file:
+		config.readfp(conf_file)
+
+	#get result
+	result = config.get(block, key)
+	
+	return result
